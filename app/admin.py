86d0ee2de0888c9e8b59dc340fbc38c8797cfe28
Jean Dias <diasjeanferreira@gmail.com>
2024-11-05 16:35:30 -0300

    todos os models criados

---
+++ app/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import *
+
+admin.site.register(UF)
+admin.site.register(Cidade)
+admin.site.register(Autor)
+admin.site.register(Editora)
+admin.site.register(Leitor)
+admin.site.register(Genero)
+admin.site.register(Livro)
+# Register your models here.
