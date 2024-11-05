cef65b0b7ed752ce82499ef7a0018b25d95052ae
Jean Dias <diasjeanferreira@gmail.com>
2024-11-05 16:54:27 -0300

    adicionado inline no UF

--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(UF)
+class CidadeInline(admin.TabularInline):
+    model = Cidade
+    extra = 1
+
+class UFAdmin(admin.ModelAdmin):
+    inlines = [CidadeInline]
+
+
+admin.site.register(UF, UFAdmin)
 admin.site.register(Cidade)
 admin.site.register(Autor)
 admin.site.register(Editora)
