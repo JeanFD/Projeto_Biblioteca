21054e561f58bc1c8cf42c8ffc3ebae3195df81c
Jean Dias <102756457+JeanFD@users.noreply.github.com>
2024-12-03 14:59:22 -0300

    finalizar projeto

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,14 @@ from .views import *
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('livros/', LivrosView.as_view(), name='livros')
+    path('pessoasfisicas/', PessoasFisicasView.as_view(), name='pessoasfisicas'),
+    path('pessoasjuridicas/', PessoasJuridicasView.as_view(), name='pessoasjuridicas'),
+    path('autores/', AutoresView.as_view(), name='autores'),
+    path('editoras/', EditorasView.as_view(), name='editoras'),
+    path('usuarios/', UsuariosView.as_view(), name='usuarios'),
+    path('generos/', GenerosView.as_view(), name='generos'),
+    path('livros/', LivrosView.as_view(), name='livros'),
+    path('ufs/', UfsView.as_view(), name='ufs'),
+    path('cidades/', CidadesView.as_view(), name='cidades'),
+    path('emprestimos/', EmprestimosView.as_view(), name='emprestimos')
 ]
--- app/views.py
+++ app/views.py
@@ -4,7 +4,56 @@ from .models import *
 # Create your views here.
 class IndexView(View):
     def get(self, request):
-        return render(request, 'livros.html')
+        return render(request, 'index.html')
+    
+class PessoasFisicasView(View):
+    def get(self, request):
+        contexto = {
+            'pessoasfisicas': PessoaFisica.objects.all(),
+        }
+        return render(request, 'pessoasfisicas.html', contexto)
+    
+class PessoasJuridicasView(View):
+    def get(self, request):
+        contexto = {
+            'pessoasjuridicas': PessoaJuridica.objects.all(),
+        }
+        return render(request, 'pessoasjuridicas.html', contexto)
+    
+class AutoresView(View):
+    def get(self, request):
+        contexto = {
+            'autores': Autor.objects.all(),
+        }
+        return render(request, 'autores.html', contexto)
+    
+class EditorasView(View):
+    def get(self, request):
+        contexto = {
+            'editoras': Editora.objects.all(),
+        }
+        return render(request, 'editoras.html', contexto)
+
+class UsuariosView(View):
+    def get(self, request):
+        contexto = {
+            'usuarios': Usuario.objects.all(),
+        }
+        return render(request, 'usuarios.html', contexto)
+    
+class GenerosView(View):
+    def get(self, request):
+        contexto = {
+            'generos': Genero.objects.all(),
+        }
+        return render(request, 'generos.html', contexto)
+    
+class AutoresView(View):
+    def get(self, request):
+        contexto = {
+            'autores': Autor.objects.all(),
+        }
+        return render(request, 'autores.html', contexto)
     
 class LivrosView(View):
     def get(self, request):
@@ -13,3 +62,24 @@ class LivrosView(View):
             'emprestimo': Emprestimo.objects.all()
         }
         return render(request, 'livros.html', contexto)
+    
+class UfsView(View):
+    def get(self, request):
+        contexto = {
+            'ufs': UF.objects.all(),
+        }
+        return render(request, 'ufs.html', contexto)
+    
+class CidadesView(View):
+    def get(self, request):
+        contexto = {
+            'cidades': Cidade.objects.all(),
+        }
+        return render(request, 'cidades.html', contexto)
+    
+class EmprestimosView(View):
+    def get(self, request):
+        contexto = {
+            'emprestimos': Emprestimo.objects.all(),
+        }
+        return render(request, 'emprestimos.html', contexto)
